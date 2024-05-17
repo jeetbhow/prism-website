bdbd6b6f96c8f227f354d8e42263dfccccef4073
Jeet Bhowmik <jeetbhowm@gmail.com>
2024-05-17 15:05:30 -0700

    feat:Add card tab switching

--- main.js
+++ main.js
@@ -4,18 +4,39 @@ import "aos/dist/aos.css";
 
 AOS.init();
 
-const hamburgerMenu = document.getElementById("hamburger");
+// const hamburgerMenu = document.getElementById("hamburger");
+const pricingTabs = document.querySelectorAll(".pricing-tab");
+const priceCards = document.querySelectorAll(".price-card");
 
-hamburgerMenu.addEventListener("click", () => {
-  // Toggle the button animation
-  hamburgerMenu.toggleAttribute("open");
+pricingTabs.forEach((tab) =>
+  tab.addEventListener("click", handlePriceTabClick),
+);
 
-  // Toggle the navbar
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("hidden");
-  nav.classList.toggle("h-screen");
+// hamburgerMenu.addEventListener("click", () => {
+//   // Toggle the button animation
+//   hamburgerMenu.toggleAttribute("open");
 
-  // Lock the menu
-  const body = document.querySelector("body");
-  body.classList.toggle("overflow-hidden");
-});
+//   // Toggle the navbar
+//   const nav = document.querySelector("nav");
+//   nav.classList.toggle("hidden");
+//   nav.classList.toggle("h-screen");
+
+//   // Lock the menu
+//   const body = document.querySelector("body");
+//   body.classList.toggle("overflow-hidden");
+// });
+
+function handlePriceTabClick(e) {
+  pricingTabs.forEach((tab) => tab.removeAttribute("active"));
+  e.target.toggleAttribute("active");
+
+  priceCards.forEach((card) => card.classList.add("hidden"));
+
+  const targetKey = e.target.getAttribute("key");
+  for (const card of priceCards) {
+    if (card.getAttribute("key") === targetKey) {
+      card.classList.remove("hidden");
+      card.classList.add("flex");
+    }
+  }
+}
